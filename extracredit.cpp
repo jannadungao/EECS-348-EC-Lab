cc4fbe6bbaea3374fc475de4c6f6e7d2d5f4ee84
jannadungao <jannadungao2@gmail.com>
2024-11-25 13:09:04 -0600

    Edits

--- extracredit.cpp
+++ extracredit.cpp
@@ -35,17 +35,21 @@ int main() {
 double extractNumeric(input) {
     int length = input.length();
     double new_num;
-    string number[length];
+    bool neg = false;
+    double dec_index = 1.0;
 
     // Check if valid input
     int sign_count = 0;
     int dec_count = 0;
+    if (index[0] == '.' || index[length - 1] == '.') {
+        return -999999.99;
+    }
 
     for (int i = 0; i < length; i++) {
         if (input[i] != '0' && input[i] != '1' && input[i] != '2' && input[i] != '3' && input[i] != '4' &&
             input[i] != '5' && input[i] != '6' && input[i] != '7' && input[i] != '8' && input[i] != '9' && 
             input[i] != '.' && input[i] != '+' && input[i] != '-') {
-                new_num = -999999.99;
+                return -999999.99;
         } else if (input[i] == '+' || input[i] == '-'){
             sign_count++;
         } else if (input[i] == '.') {
@@ -53,11 +57,32 @@ double extractNumeric(input) {
         }
     }
 
-    if (sign_count > 1 || dec_count > 1) {
-        new_num = -999999.99
+    for (int i = 1; i < length; i++) {
+        if (input[i] == '-' || input[i] == '+') {
+            return -999999.99;
+        }
     }
-    return new_num
+
+    if (sign_count > 1 || dec_count > 1 || length == 0 || length > 20) {
+        return -999999.99;
+    }
 
     // if valid input
+    for (int i = 0; i < length; i++) {
+        if (input[i] == '-') {
+            neg = true;
+        } else if (input[i] == '+') {
+            ;
+        } else if (input[i] == '.') {
+            dec_index /= 10.0;
+            new_num += dec_index * (index[i] - '0');
+        } else {
+            new_num = (index[i] - '0') + (new_num * 10);
+        }
+    }    
+    if (neg == true) {
+        new_num *= -1;
+    }
     
+    return new_num
 }
